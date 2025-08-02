7d8fad992ab14b7d4ac4fe7f516b35a84f547961
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-02 04:50:38 -0700

    Sequence_Problem

---
+++ C#_Fundamentals/IntroductionToProgramming/02_Sequence/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int number = 2;
+        Console.WriteLine("First 100 integers, with 3rd integer is negative are: ");
+
+for(int i = 1; i <= 100; i++)
+{
+    //Every third integer starting from 4 should be negative
+    if((i - 1) % 3 == 0 && i !=1)
+    {
+        Console.Write(-number + " ");
+    }
+    else
+    {
+        Console.Write(number+ " ");
+    }
+    number++;
+
+    if(i % 10 == 0)
+    {
+        Console.WriteLine();
+    }
+}
+    }
+}
