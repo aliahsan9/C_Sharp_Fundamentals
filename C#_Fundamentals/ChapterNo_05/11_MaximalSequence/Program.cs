c282894047f5c4950e8f79e627ea7103ab7fae37
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-12 06:49:10 -0700

    Add new files

---
+++ C#_Fundamentals/ChapterNo_05/11_MaximalSequence/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] array = { 1, 1, 2, 3, 2, 2, 2, 1 };
+
+        int maxCount = 1;
+        int currentCount = 1;
+        int number = array[0];
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] == array[i - 1])
+            {
+                currentCount++;
+                if (currentCount > maxCount)
+                {
+                    maxCount = currentCount;
+                    number = array[i];
+                }
+            }
+            else
+            {
+                currentCount = 1; // reset for new sequence
+            }
+        }
+
+        Console.Write("Maximal sequence: ");
+        for (int i = 0; i < maxCount; i++)
+        {
+            Console.Write(number + " ");
+        }
+    }
+}
