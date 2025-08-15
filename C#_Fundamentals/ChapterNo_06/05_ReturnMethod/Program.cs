43468d94b88b6a86f0a846ed27d260df4dc5a625
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-15 08:51:23 -0700

    Return method

---
+++ C#_Fundamentals/ChapterNo_06/05_ReturnMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static int PrintArray(int num1, int num2)
+    {
+        int product = num1 * num2;
+        return product;
+    }
+    public static void Main(string[] args)
+    {
+        int num1 = 3, num2 = 5;
+        int product = PrintArray(num1, num2);
+        Console.WriteLine("Product of both numbers is:"+ product);
+    }
+}
