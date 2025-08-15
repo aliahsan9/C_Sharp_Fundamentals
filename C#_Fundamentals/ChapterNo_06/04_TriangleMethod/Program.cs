3d3408688999b284cbf37ccaa8b89fc70f38e508
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-15 08:50:51 -0700

    Triangle method

---
+++ C#_Fundamentals/ChapterNo_06/04_TriangleMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void PrintTriangle(int[] nums)
+    {
+        int n = nums.Length;
+        for(int i = 0; i <= n; i++)
+        {
+          for(int j = 0; j < i; j++)
+          {
+            Console.Write(nums[j] + " ");
+          }
+          Console.WriteLine();
+        }
+    }
+    public static void Main(string[] args)
+    {
+        int[] nums = {1, 2, 3, 4, 5};
+        PrintTriangle(nums);
+    }
+}
