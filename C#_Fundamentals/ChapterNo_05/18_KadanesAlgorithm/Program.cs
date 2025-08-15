6256515be73a500d86809101590dd85641e6ba69
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-15 02:38:48 -0700

    Print Sum of sub arrays using Kadanes algorithm for best Time complexity

---
+++ C#_Fundamentals/ChapterNo_05/18_KadanesAlgorithm/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    public static int MaxSumSubarray(int[] nums)
+    {
+        int maxSum = int.MinValue;
+        int currentSum = 0;
+
+        foreach (int val in nums)
+        {
+            currentSum += val;
+            maxSum = Math.Max(currentSum, maxSum);
+
+            if (currentSum < 0)
+            {
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] array = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        int MaxSum = MaxSumSubarray(array);
+        Console.WriteLine($"Maximum Sum of Subarray is: {MaxSum}");
+    }
+}
