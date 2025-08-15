6bdd6b3f465b1cce281826df26217420295376cc
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-15 08:48:55 -0700

    Books price

---
+++ C#_Fundamentals/ChapterNo_06/02_BooksPriceMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void PrintTotalAmountOfBooks(decimal[] prices)
+    {
+        decimal totalPrice = 0;
+
+        foreach (decimal singleBookPrice in prices)
+        {
+            totalPrice += singleBookPrice;
+        }
+        Console.WriteLine("Total amount of books is:"+ totalPrice);
+    }
+    public static void Main(string[] args)
+    {
+        decimal[] bookPrice = {250.9m, 440.9m, 109.9m};
+        PrintTotalAmountOfBooks(bookPrice);
+    }
+}
