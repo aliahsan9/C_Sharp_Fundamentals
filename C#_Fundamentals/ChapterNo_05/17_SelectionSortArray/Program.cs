dfeec7752f3a7b30b00870dd9fc861826c332bc9
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-14 23:40:48 -0700

    Selection Sort array

---
+++ C#_Fundamentals/ChapterNo_05/17_SelectionSortArray/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] array = { 4, 5, 6, 10, 2, 1, 3, 7 };
+
+        Console.WriteLine("Before Sorting:");
+        PrintArray(array);
+        SelectionSort(array);
+        Console.WriteLine("After Sorting:");
+        PrintArray(array);
+    }
+
+    static void SelectionSort(int[] array)
+    {
+        int n = array.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++)
+            {
+                if (array[j] < array[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i)
+            {
+                int temp = array[minIndex];
+                array[minIndex] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+    static void PrintArray(int[] array)
+    {
+        foreach (int item in array)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
