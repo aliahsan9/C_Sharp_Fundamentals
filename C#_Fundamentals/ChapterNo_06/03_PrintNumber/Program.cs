12fe8a3995de531eb702a69a5db91fa6f348a253
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-15 08:50:17 -0700

    Print number

---
+++ C#_Fundamentals/ChapterNo_06/03_PrintNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void PrintSign(int number)
+    {
+        if(number < 0)
+        {
+            Console.WriteLine("Given number is negative.");
+        }
+        else
+        if(number > 0)
+        {
+            Console.WriteLine("Given Number is positive.");
+        }
+        else
+        {
+            Console.WriteLine("Given number is zero.");
+        }
+    }
+    public static void Main(string[] args)
+    {
+        int num = 2;
+        PrintSign(num);
+    }
+}
