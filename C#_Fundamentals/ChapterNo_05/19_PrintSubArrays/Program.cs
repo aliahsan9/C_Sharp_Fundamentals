f10b99642700cba33f752e479ae1af4e17acdcc6
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-15 02:15:07 -0700

    Print Sum of sub arrays using Brute force approach

---
+++ C#_Fundamentals/ChapterNo_05/19_PrintSubArrays/Program.cs
@@ -0,0 +1,23 @@
+﻿//Here we will print the sum of maximun subarray from given array using Brutr force method
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        int n = array.Length;
+        int maxSum = int.MinValue;
+        for (int i = 0; i < n; i++)
+        {
+            int currentSum = 0;
+            for (int j = i; j < n; j++)
+            {
+                currentSum += array[j];
+                maxSum = Math.Max(currentSum, maxSum);
+            }
+        }
+        Console.Write(maxSum);
+    }
+}
