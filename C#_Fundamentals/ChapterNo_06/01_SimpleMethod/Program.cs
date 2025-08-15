ff003222ba336378f802b8c70d4f25de833a703a
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-15 08:49:42 -0700

    Simple Method

---
+++ C#_Fundamentals/ChapterNo_06/01_SimpleMethod/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+       static void Main(string[] args)
+    {
+        PrintLogo();
+    }
+    public static void PrintLogo()
+    {
+        Console.WriteLine("Hello, world!");
+    }
+
+
+}
