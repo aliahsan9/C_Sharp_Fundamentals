57c656fc132f06c7f2164236abcc341257332d55
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-16 01:30:42 -0700

    Palidrome using two pointers approach

---
+++ C#_Fundamentals/ChapterNo_06/09_PalidromeUsingTwoPointers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static bool IsPalindrome(string s)
+    {
+        int left = 0, right = s.Length - 1;
+
+        while (left < right)
+        {
+            if (s[left] != s[right])
+                return false;
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(IsPalindrome("madam")); // True
+        Console.WriteLine(IsPalindrome("hello")); // False
+    }
+}
