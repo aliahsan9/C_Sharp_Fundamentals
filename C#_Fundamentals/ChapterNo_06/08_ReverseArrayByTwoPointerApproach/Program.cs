1ff653f563f451615f508ec9d5c15e0598d301e6
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-16 01:29:07 -0700

    Reverse array by two pointer approach

---
+++ C#_Fundamentals/ChapterNo_06/08_ReverseArrayByTwoPointerApproach/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void ReverseArray(int[] arr)
+    {
+        int left = 0;
+        int right = arr.Length - 1;
+
+        while (left < right)
+        {
+            // Swap
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            // Move pointers
+            left++;
+            right--;
+        }
+    }
+
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        ReverseArray(numbers);
+
+        Console.WriteLine(string.Join(", ", numbers)); // Output: 5, 4, 3, 2, 1
+    }
+}
