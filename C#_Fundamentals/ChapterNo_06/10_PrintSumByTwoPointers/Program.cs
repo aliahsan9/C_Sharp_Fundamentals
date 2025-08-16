73101761a10b40a1408e7a10ee1643906f2f903f
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-16 01:31:28 -0700

    Print sum using two pointers

---
+++ C#_Fundamentals/ChapterNo_06/10_PrintSumByTwoPointers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static bool HasPairWithSum(int[] arr, int target)
+    {
+        int left = 0, right = arr.Length - 1;
+
+        while (left < right)
+        {
+            int sum = arr[left] + arr[right];
+
+            if (sum == target)
+                return true;
+            else if (sum < target)
+                left++;
+            else
+                right--;
+        }
+        return false;
+    }
+
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 6 };
+        Console.WriteLine(HasPairWithSum(numbers, 9));  // True (3+6)
+        Console.WriteLine(HasPairWithSum(numbers, 20)); // False
+    }
+}
