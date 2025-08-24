d543c1430270eaece6ee6caa47e0772d13a2e7e0
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-24 10:18:26 -0700

    Computer started time

---
+++ C#_Fundamentals/ChapterNo_09/04_WhenComputerStarted/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Uptime
+{
+    static void Main(string[] args)
+    {
+        // Get milliseconds since system started
+        long milliseconds = Environment.TickCount64;
+
+        // Convert to TimeSpan for easy calculation
+        TimeSpan uptime = TimeSpan.FromMilliseconds(milliseconds);
+
+        Console.WriteLine("Time since computer started:");
+        Console.WriteLine($"Days   : {uptime.Days}");
+        Console.WriteLine($"Hours  : {uptime.Hours}");
+        Console.WriteLine($"Minutes: {uptime.Minutes}");
+    }
+}
