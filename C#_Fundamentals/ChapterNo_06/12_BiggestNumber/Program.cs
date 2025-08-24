85a2631d86f7e807424808410fa94b5713af9b10
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-24 10:04:40 -0700

    Biggest Number

---
+++ C#_Fundamentals/ChapterNo_06/12_BiggestNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static int GetMax(int num1, int num2)
+    {
+       return (num1 > num2) ? num1 : num2;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter num1:");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter num2:");
+        int num2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter num3:");
+        int num3 = int.Parse(Console.ReadLine());
+
+        int bigget = GetMax(GetMax(num1, num2), num3);
+        Console.WriteLine($"Biggest number is: {bigget}");
+    }
+}
