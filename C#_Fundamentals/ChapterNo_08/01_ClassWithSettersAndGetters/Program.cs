2ad81a85a01d3c4539b54de90cd47925f159d459
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-24 10:15:06 -0700

    simple class, with setters and getters methods

---
+++ C#_Fundamentals/ChapterNo_08/01_ClassWithSettersAndGetters/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+class Program
+{
+    public class Cat
+    {
+        //Field name
+        private string name;
+        //field color
+        private string color;
+
+        //Now define Setters ans Getters methods for Name, because fileds are private
+
+        public string Name
+        {
+           get
+           {
+           return this.name;
+           }
+           set
+           {
+            this.name = value;
+           }
+        }
+
+        //Now define Setters adn Getters for Color because filed of color is private
+        
+        public string Color
+        {
+            get
+            {
+                return this.color;
+            }
+            set
+            {
+                 this.color = value;
+            }
+        }
+        //Default Constructor
+
+        public Cat()
+        {
+            this.name = "UnNamed";
+            this.color = "Gray";
+        }
+       // Constructor with parameters
+
+       public Cat(string name, string color)
+       {
+        this.name = name;
+        this.color = color;
+       }
+
+     //Method sayMeou
+    public void SayMeou()
+      {
+    Console.WriteLine("Cat {0} said meou", name);
+      }
+    }
+
+    static void Main(string[] args)
+    {
+        Cat firstCat = new Cat();
+        firstCat.Name = "Tony";
+        firstCat.SayMeou();
+
+        Cat secondCat = new Cat("Pepy", "red");
+        secondCat.SayMeou();
+        Console.WriteLine("Cat {0} is {1}",secondCat.Name, secondCat.Color);
+    }
+}
