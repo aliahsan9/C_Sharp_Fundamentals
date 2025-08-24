500dd1dff43daa0261ccda58626f02f3d65bf299
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-24 10:17:52 -0700

    Today print

---
+++ C#_Fundamentals/ChapterNo_09/03_DayOfWeekToday/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Today
+{
+    public string PrintToday()
+    {
+        DayOfWeek today = DateTime.Now.DayOfWeek; // fixed typo
+        return today.ToString();
+    }
+
+    static void Main(string[] args)
+    {
+        Today todayDay = new Today();
+        Console.WriteLine("Today is: " + todayDay.PrintToday());
+    }
+}
