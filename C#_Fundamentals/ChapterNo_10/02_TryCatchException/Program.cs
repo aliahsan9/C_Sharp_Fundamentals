34dee30ce879e427d06cca8f08a37984d5743b18
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-24 10:14:09 -0700

    Exception handing

---
+++ C#_Fundamentals/ChapterNo_10/02_TryCatchException/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void ReadFile(string fileName)
+    {
+        try
+        {
+            using (TextReader reader = new StreamReader(fileName))
+            {
+                string line = reader.ReadToEnd(); // reads entire file
+                Console.WriteLine(line);
+            }
+        }
+        catch (FileNotFoundException fnfe)
+        {
+            Console.WriteLine("The file '{0}' is not found.", fileName);
+        }
+        catch (IOException ioe)
+        {
+            // Exception handler for other input/output exceptions
+            Console.WriteLine("An IO Exception occurred:");
+            Console.WriteLine(ioe.StackTrace);
+        }
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter file path: ");
+        string path = Console.ReadLine();
+        ReadFile(path);
+    }
+}
