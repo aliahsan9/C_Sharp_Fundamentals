95730ea674c215529bdbe5601d7964b68ef0c93a
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-24 10:17:15 -0700

    Print random numbers

---
+++ C#_Fundamentals/ChapterNo_09/02_PrintRandomNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class RandomNumbers
+{
+    private Random random = new Random();
+
+    public int GenerateNumbers()
+    {
+        return random.Next(100, 201);
+    }
+    public void PrintRandomNumbers(int count)
+    {
+        for(int i = 0; i < count; i++)
+        {
+            Console.Write(GenerateNumbers()+ " ");
+        }
+    }
+}
+class Program
+{
+    public static void Main(String[] args)
+    {
+        RandomNumbers rng = new RandomNumbers();
+        rng.PrintRandomNumbers(10);
+    }
+}
