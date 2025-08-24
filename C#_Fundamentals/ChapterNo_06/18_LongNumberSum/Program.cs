ae5dc6e598cdf6935f10db1009bbdb3deddad752
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-24 10:08:27 -0700

    LongNumber Sum

---
+++ C#_Fundamentals/ChapterNo_06/18_LongNumberSum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static List<int> SumBigIntegers(int[] num1, int[] num2)
+{
+    List<int> result = new List<int>();
+    int carry = 0, maxLen = Math.Max(num1.Length, num2.Length);
+
+    for (int i = 0; i < maxLen; i++)
+    {
+        int digit1 = i < num1.Length ? num1[i] : 0;
+        int digit2 = i < num2.Length ? num2[i] : 0;
+        int sum = digit1 + digit2 + carry;
+
+        result.Add(sum % 10);
+        carry = sum / 10;
+    }
+
+    if (carry > 0)
+        result.Add(carry);
+
+    return result; // digits in reversed order
+}
+    static void Main()
+    {
+    
+}
+}
