414db0c64854b89710beff2f0003344823d80b5a
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-24 10:12:19 -0700

    Print reverse string

---
+++ C#_Fundamentals/ChapterNo_11/02_PrintReverseString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Reverse
+{
+    public static void PrintReverseArray(string s)
+    {
+        char[] charArray = s.ToCharArray();
+
+        Array.Reverse(charArray);
+
+        string reverse = new string(charArray);
+
+        Console.WriteLine($"Reversed string is: {reverse}");
+    }
+
+    static void Main(string[] args)
+    {
+        string name = "Ali Ahsan";
+        PrintReverseArray(name);
+    }
+}
