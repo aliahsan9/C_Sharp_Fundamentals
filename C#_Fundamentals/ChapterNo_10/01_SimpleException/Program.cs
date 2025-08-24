e002cfc62b313352d9ccb6e587530d3e96bb71df
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-24 10:13:35 -0700

    Exception handing

---
+++ C#_Fundamentals/ChapterNo_10/01_SimpleException/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class ExceptionsDemo
+{
+    static void Main()
+    {
+    string fileName = "WrongTextFile.txt";
+    ReadFile(fileName);
+    }
+    static void ReadFile(string fileName)
+  {
+    TextReader reader = new StreamReader(fileName);
+    string line = reader.ReadLine();
+    Console.WriteLine(line);
+    reader.Close();
+}
+}
