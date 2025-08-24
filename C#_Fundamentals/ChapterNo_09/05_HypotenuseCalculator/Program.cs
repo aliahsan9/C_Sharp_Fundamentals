6d27f38847a2c4032765f4f3123889cde85cd3ba
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-24 10:19:07 -0700

    Claculator

---
+++ C#_Fundamentals/ChapterNo_09/05_HypotenuseCalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class HypotenuseCalculator
+{
+    static void Main(string[] args)
+    {
+        // Input from user
+        Console.Write("Enter the length of side A: ");
+        double sideA = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the length of side B: ");
+        double sideB = Convert.ToDouble(Console.ReadLine());
+
+        // Hypotenuse formula: √(a² + b²)
+        double hypotenuse = Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2));
+
+        // Output result
+        Console.WriteLine("The hypotenuse of the triangle is: " + hypotenuse);
+    }
+}
