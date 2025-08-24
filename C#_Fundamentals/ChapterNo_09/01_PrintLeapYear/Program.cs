1de4e79fd898b0f69c4638dd2953792d75028ab5
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-24 10:16:44 -0700

    Print leap year

---
+++ C#_Fundamentals/ChapterNo_09/01_PrintLeapYear/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class LeapYear
+{
+  public string PrintLeapYear(int year)
+  {
+    if (year % 4 == 0)
+    {
+      return "Leap Year";
+    }
+    return "Not a Leap Year!";
+  }
+
+  public static void Main(string[] args)
+  {
+    Console.WriteLine("Enter any year to check either it is leap or not:");
+    int year = int.Parse(Console.ReadLine());
+
+    //Make an object 
+    LeapYear leap = new LeapYear();
+
+    string result = leap.PrintLeapYear(year);
+    Console.WriteLine($"{year} is a {result}");
+  }
+}
