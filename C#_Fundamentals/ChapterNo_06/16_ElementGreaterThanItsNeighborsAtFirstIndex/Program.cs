bc12f6960b0e3328eb184979e70c31c18b43fef2
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-25 00:05:06 -0700

    Greater element than its neighbors first index

---
+++ C#_Fundamentals/ChapterNo_06/16_ElementGreaterThanItsNeighborsAtFirstIndex/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    // Method to find first index where element is greater than its neighbors
+    static int FirstPeakPosition(int[] arr)
+    {
+        for (int i = 1; i < arr.Length - 1; i++)
+        {
+            if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1])
+                return i;
+        }
+        return -1;
+    }
+
+    static void Main()
+    {
+        int[] arr = { 1, 3, 5, 4, 2, 7, 6 };
+        int pos = FirstPeakPosition(arr);
+
+        if (pos != -1)
+            Console.WriteLine($"First peak element found at position {pos} (value = {arr[pos]})");
+        else
+            Console.WriteLine("No element greater than its two neighbors found.");
+    }
+}
