4ff832bbc8a91220d9b72084cf4cf22dc26ed69b
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-24 23:56:27 -0700

    Fibonacci recursion

---
+++ C#_Fundamentals/ChapterNo_07/01_FibonacciRecursion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static int GetFibonacci(int num)
+    {
+        //Base Case
+        if(num <= 2)
+        {
+            return 1;
+        }
+        return GetFibonacci(num - 1) + GetFibonacci(num - 2);
+    }
+    static void Main(String[] args)
+    {
+        int num = 10;
+        Console.Write($"Fibonacci Sum of {num} is: {GetFibonacci(num)}");
+    }
+}
