7e3512872c3ab09023d7a5ff4d4677db8b14dfb4
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-25 00:02:43 -0700

    Polynomial sum

---
+++ C#_Fundamentals/ChapterNo_06/22_PolynomialsSum/Program.cs
@@ -0,0 +1,13 @@
+﻿static int[] SumPolynomials(int[] p1, int[] p2)
+{
+    int maxLen = Math.Max(p1.Length, p2.Length);
+    int[] result = new int[maxLen];
+
+    for (int i = 0; i < maxLen; i++)
+    {
+        int coef1 = i < p1.Length ? p1[i] : 0;
+        int coef2 = i < p2.Length ? p2[i] : 0;
+        result[i] = coef1 + coef2;
+    }
+    return result;
+}
