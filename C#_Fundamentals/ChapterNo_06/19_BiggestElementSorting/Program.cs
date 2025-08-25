c62409ec52881015c55c90acb0a225581fbc1f34
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-25 00:01:06 -0700

    Biggest elemeny sorting

---
+++ C#_Fundamentals/ChapterNo_06/19_BiggestElementSorting/Program.cs
@@ -0,0 +1,21 @@
+﻿static int FindMaxIndex(int[] arr, int start)
+{
+    int maxIndex = start;
+    for (int i = start + 1; i < arr.Length; i++)
+    {
+        if (arr[i] > arr[maxIndex])
+            maxIndex = i;
+    }
+    return maxIndex;
+}
+
+static void SortDescending(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        int maxIndex = FindMaxIndex(arr, i);
+        int temp = arr[i];
+        arr[i] = arr[maxIndex];
+        arr[maxIndex] = temp;
+    }
+}
