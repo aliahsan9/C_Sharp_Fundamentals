bb6974cde9866098a84e79a5f0561f638f816647
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-24 23:57:14 -0700

    Factorial by recursion

---
+++ C#_Fundamentals/ChapterNo_07/02_FactorialByRecursion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static int GetFactorial(int n)
+    {
+        // Base Case
+        if(n == 0)
+        {
+            return 1;
+        }
+        return n* GetFactorial(n - 1);
+    }
+    static void Main(String[] args)
+    {
+        Console.Write("Enter any integer to get factorial:");
+        int num = int.Parse(Console.ReadLine());
+        Console.Write(GetFactorial(num));
+    }
+}
