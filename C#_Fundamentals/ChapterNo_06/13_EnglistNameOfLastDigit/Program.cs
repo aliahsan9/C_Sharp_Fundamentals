46339e94283d577142574520b12a9fa6f81b964d
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-24 23:59:56 -0700

    english name pof last digit

---
+++ C#_Fundamentals/ChapterNo_06/13_EnglistNameOfLastDigit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static string GetLastDigitName(int number)
+    {
+        int lastDigit = Math.Abs(number % 10);
+        string[] digitNames =
+                {
+            "zero", "one", "two", "three", "four",
+            "five", "six", "seven", "eight", "nine"
+        };
+
+        return digitNames[lastDigit];
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter any velaue:");
+        int num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"English name of last digit is: {GetLastDigitName(num)}");
+    }
+}
