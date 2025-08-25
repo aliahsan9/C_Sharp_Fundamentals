b7717cfc1ee9f333165d25cd0e90ee8185283981
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-24 23:57:46 -0700

    Factorial by iteratioin

---
+++ C#_Fundamentals/ChapterNo_07/03_FactorialByIteration/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static int GetFactorial(int n)
+    {
+        int result = 1;
+
+        for(int i = 1; i <= n; i++)
+        {
+            result*=i;
+        }
+        return result;
+    }
+    static void Main(String[] args)
+    {
+        Console.Write("Enter any integer to check it's factorial:");
+        int num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Factorial of {num} is: {GetFactorial(num)}");
+    }
+}
