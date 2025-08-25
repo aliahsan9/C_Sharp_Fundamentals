3aa313b6c6edc884e6cba6d638efb332bb3347b0
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-25 00:03:18 -0700

    Polynomials product

---
+++ C#_Fundamentals/ChapterNo_06/23_PolynomialsProduct/Program.cs
@@ -0,0 +1,13 @@
+﻿static int[] MultiplyPolynomials(int[] p1, int[] p2)
+{
+    int[] result = new int[p1.Length + p2.Length - 1];
+
+    for (int i = 0; i < p1.Length; i++)
+    {
+        for (int j = 0; j < p2.Length; j++)
+        {
+            result[i + j] += p1[i] * p2[j];
+        }
+    }
+    return result;
+}
