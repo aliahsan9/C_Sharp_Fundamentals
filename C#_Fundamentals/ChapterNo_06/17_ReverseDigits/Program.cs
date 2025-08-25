132b3b2107c633f22e950e921a161bf0f386cd31
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-25 00:00:33 -0700

    Reverse digits

---
+++ C#_Fundamentals/ChapterNo_06/17_ReverseDigits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+static void PrintReversedDigits(int number)
+{
+    while (number > 0)
+    {
+        Console.Write(number % 10);
+        number /= 10;
+    }
+    Console.WriteLine();
+}
+    static void Main(string[] args)
+    {
+        int num = 123;
+        Console.WriteLine("Reversed of given integer is:");
+        PrintReversedDigits(num);
+    }
+}
