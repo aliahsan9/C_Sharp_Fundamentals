c06188dd11d7eae4b5875205e7ae492170575ec9
Ali Ahsan <aliahsan8751@gmail.com>
2025-10-07 01:43:33 -0700

    count occurences

---
+++ C#_Fundamentals/ChapterNo_14/02_RemoveSequence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] sequence = {1, 2, 3, 4 , 5, 6, 7, 7, 8, 8};
+
+        Dictionary<int, int> countMap = new Dictionary<int, int>();
+
+        foreach(int num in sequence) 
+        {
+            if(countMap.ContainsKey(num))
+            countMap[num]++;
+            else 
+            countMap[num] = 1;
+        }
+        List<int> finalNums = new List<int>();
+
+        foreach(int num in sequence)
+        {
+           if(countMap[num] % 2 == 0)
+           {
+           finalNums.Add(num);
+           }
+        }
+        Console.WriteLine("Result Sequence is: {" + string.Join(", ", finalNums ) +  "}");
+    }
+}
