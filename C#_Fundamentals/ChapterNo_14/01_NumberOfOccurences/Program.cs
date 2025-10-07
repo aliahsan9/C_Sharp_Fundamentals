c1c5736ff5a60c3c56a6dde580cfe49bf4d8e644
Ali Ahsan <aliahsan8751@gmail.com>
2025-10-07 01:43:11 -0700

    count occurences

---
+++ C#_Fundamentals/ChapterNo_14/01_NumberOfOccurences/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main(string[] args)
+    { 
+       int[] array = {1, 2, 3, 4, 4, 5, 6, 7, 2, 1, 2};
+
+       Dictionary<int, int> countMap = new Dictionary<int, int>();
+
+       foreach(int num in array)
+       {
+          if(countMap.ContainsKey(num))
+          countMap[num]++;
+          else 
+          countMap[num] = 1;
+       }
+       //Display Result
+       foreach(var item in countMap)
+       {
+        Console.WriteLine($"{item.Key} -> {item.Value} times");
+       }
+    }
+}
