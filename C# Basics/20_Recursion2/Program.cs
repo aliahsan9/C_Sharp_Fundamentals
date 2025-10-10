1fa1c95b2109169070da412ecc2f4b14f3df4d0b
Ali Ahsan <aliahsan8751@gmail.com>
2025-10-10 07:45:03 -0700

    fibonacci numbers

---
+++ C# Basics/20_Recursion2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+class Program
+{
+    static int Fibonacci(int n)
+    {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+    static void Main(string[] args)
+    {
+        int n = 5;
+        Console.WriteLine("Fibonacci numbers are:" + Fibonacci(n));
+    }
+}
