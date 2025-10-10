16cfc4c53cc87cf0a1500d6565fe18298d1afe8f
Ali Ahsan <aliahsan8751@gmail.com>
2025-10-10 06:21:07 -0700

    fibonacci numbers

---
+++ C# Basics/19_FibonnaciNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = 10;
+        int a = 0, b = 1, next;
+        Console.WriteLine("First 10 Fibonacci Numbers are:");
+
+        Console.WriteLine(a + " " + b + " ");
+
+        for (int i = 3; i <= n; i++)
+        {
+            next = a + b;
+            Console.Write(next + " ");
+            a = b;
+            b = next;
+        }
+        Console.WriteLine();
+    }
+}
