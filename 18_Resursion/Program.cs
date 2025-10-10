3ac83647d22015a203db2326abb1e05ba24dd521
Ali Ahsan <aliahsan8751@gmail.com>
2025-10-10 01:02:14 -0700

    Sum of digits

---
+++ 18_Resursion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static int SumOfDigits(int n)
+    {
+        if (n == 0)
+            return 0;
+        else
+            return (n % 10) + SumOfDigits(n / 10);
+    }
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Enter any digit:");
+        int n = int.Parse(Console.ReadLine());
+
+        int result = SumOfDigits(n);
+        Console.WriteLine(result);
+    }
+}
