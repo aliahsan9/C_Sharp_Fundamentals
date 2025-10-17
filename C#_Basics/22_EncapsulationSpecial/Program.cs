c09667b8a7ec32147d4acee2f3f099cf624b789b
Ali Ahsan <aliahsan8751@gmail.com>
2025-10-17 02:50:18 -0700

    encapsulation

---
+++ C#_Basics/22_EncapsulationSpecial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class BankAccount
+{
+   private double balance;
+
+   //public property to set value
+
+   public double Balance
+   {
+      get {return balance;}
+      set {balance = value;}
+   }
+   //Deposit method 
+   public void Deposit(double amount)
+   {
+     if(amount > 0)
+     balance += amount;
+   }
+}
+class Program
+{
+public static void Main()
+{
+  BankAccount acc = new BankAccount();
+  acc.Deposit(156789);
+  Console.WriteLine($"Balance: {acc.Balance}");
+}
+}
