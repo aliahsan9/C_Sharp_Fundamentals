e384d8d9cc8485d74fc6550628cc8554d48945e2
Ali Ahsan <aliahsan8751@gmail.com>
2025-10-17 03:02:19 -0700

    encapsulation

---
+++ C#_Basics/23_EncapsulationShortForm/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Student
+{
+    public string Name {get; set;}
+    public int RollNumber {get; private set;}
+
+    //public method for rollNumber
+    public Student(int rollNumber)
+    {
+        RollNumber = rollNumber;
+    }
+}
+class Program
+{
+static void Main(string[] args)
+{
+    Student s = new Student(10);
+    s.Name = "Ali Ahsan";
+    Console.WriteLine($"Name: {s.Name}, Roll Number: {s.RollNumber}");
+}
+}
