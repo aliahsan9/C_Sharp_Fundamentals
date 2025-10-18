684f7c44999d24b708cca7714861a45b0f83ae07
Ali Ahsan <aliahsan8751@gmail.com>
2025-10-18 01:54:23 -0700

    abstraction using classes

---
+++ C#_Basics/25_AbstractionUsingClass/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+abstract class Shape
+{
+    public abstract void Draw();
+
+    public void AreaOfShape()
+    {
+        Console.WriteLine("Claculating area of given shape...");
+    }
+}
+class Circle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a Circle...");
+    }
+}
+class Reactangle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a reactangle...");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Shape circle = new Circle();
+        circle.AreaOfShape();
+        circle.Draw();
+
+        Shape rect = new Reactangle();
+        rect.Draw();
+    }
+}
