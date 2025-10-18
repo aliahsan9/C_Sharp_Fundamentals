4eed1c30df5e20de2080b623ea7f45ad0f191305
Ali Ahsan <aliahsan8751@gmail.com>
2025-10-18 01:59:18 -0700

    abstraction using interface

---
+++ C#_Basics/26_AbstractionUsingInterface/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+interface IShape
+{
+    void Draw();  //Abstract by default
+}
+
+class Circle : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("Circle is Drawing...");
+    }
+}
+class Reactangle : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("Reactangle is Drawing...");
+    }
+}
+class Program
+{
+    static void Main(string[]args)
+    {
+        IShape circle = new Circle();
+        circle.Draw();
+
+        IShape rect = new Reactangle();
+        rect.Draw();
+    }
+}
