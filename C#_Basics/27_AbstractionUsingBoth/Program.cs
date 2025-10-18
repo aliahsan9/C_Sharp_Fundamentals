896f7ca7ad679fcafc244065f01a78e924f71c2c
Ali Ahsan <aliahsan8751@gmail.com>
2025-10-18 02:32:12 -0700

    Abstraction

---
+++ C#_Basics/27_AbstractionUsingBoth/Program.cs
@@ -0,0 +1,38 @@
+﻿public interface IVehicle
+{
+    void Start();
+}
+
+public abstract class Vehicle
+{
+    public abstract void Drive();
+
+    public void Stop()
+    {
+        Console.WriteLine("Vehicle stopped.");
+    }
+}
+
+public class Car : Vehicle, IVehicle
+{
+    public override void Drive()
+    {
+        Console.WriteLine("Car is driving...");
+    }
+
+    public void Start()
+    {
+        Console.WriteLine("Car started.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Car car = new Car();
+        car.Start();
+        car.Drive();
+        car.Stop();
+    }
+}
