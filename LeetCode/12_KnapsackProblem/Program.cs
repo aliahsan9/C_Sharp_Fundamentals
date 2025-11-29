76a34ed2972be8bbb477fba741e4d1d3ef956403
Ali Ahsan <aliahsan8751@gmail.com>
2025-11-29 01:31:57 -0800

    new problem using knapsack

---
+++ LeetCode/12_KnapsackProblem/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Item
+{
+    public int Value;
+    public int Weight;
+    public Item(int v, int w)
+    {
+        Value = v;
+        Weight = w;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Item[] items = {
+            new Item(60, 10),
+            new Item(100, 20),
+            new Item(120, 30)
+        };
+        int capacity = 50;
+        z
+            {
+                capacity -= item.Weight;
+                totalValue += item.Value;
+            }
+            else
+            {
+                totalValue += item.Value * ((double)capacity / item.Weight);
+                break;
+            }
+        }
+        Console.WriteLine("Maximum value in knapsack: " + totalValue);
+    }
+}
