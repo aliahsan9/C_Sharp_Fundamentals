e1e865c4c414e76647c9d853cf2ebb74dbfef161
Ali Ahsan <aliahsan8751@gmail.com>
2025-11-29 22:22:06 -0800

    Linked list simple code

---
+++ LinkedList/01_SimpleLinkedList/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Node
+{
+    public int Data;
+    public Node Next;
+
+    // Constructor
+    public Node(int data)
+    {
+        Data = data;
+        Next = null;
+    }
+}
+
+//linked list 
+class LinkedList
+{
+    public Node Head;
+
+    //Add a new Node at the end of the Linekd List
+    public void Add(int data)
+    {
+        Node newNode = new Node(data);
+
+        if(Head == null)
+        {
+            Head = newNode;
+            return;
+        }
+        Node temp = Head;
+        while(temp.Next != null)
+        {
+            temp = temp.Next;
+        }
+        temp.Next = newNode;
+    }
+    //Print all nodes of the linked list
+    public void PrintList()
+    {
+        Node temp = Head;
+        while(temp != null)
+        {
+            Console.Write(temp.Data+ " -> ");
+            temp = temp.Next;
+        }
+        Console.WriteLine("null");
+    }
+}
+//Main program
+class program
+{
+    static void Main(string[] args)
+    {
+        LinkedList list = new LinkedList();
+        list.Add(10);
+        list.Add(20);
+        list.Add(30);
+
+        list.PrintList();
+    }
+}
