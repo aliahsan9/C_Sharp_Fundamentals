7d09438391d5179ebd50c87f305f232444acbf1e
Ali Ahsan <aliahsan8751@gmail.com>
2025-12-04 04:40:41 -0800

    Reverse String

---
+++ Stack/02_ReverseString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+class ReverseUsingStack
+{
+    public static string ReverseString(string input)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        //Push characters into the stack
+        foreach(char ch in input)
+        {
+            stack.Push(ch);
+        }
+
+        //Pop characters to form the reverses string
+        string reversed = "";
+        while(stack.Count > 0)
+        {
+            reversed += stack.Pop();
+        }
+        return reversed;
+    }
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Enter any String:");
+        string str = Console.ReadLine();
+
+        string output = ReverseString(str);
+        Console.WriteLine("Reversed String is: "+ output);
+        
+        Console.ReadLine();
+    }
+}
