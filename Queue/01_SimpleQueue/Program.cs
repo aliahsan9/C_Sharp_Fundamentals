8d2b64039c0063dec1adffbbce4148b6d25c92d2
Ali Ahsan <aliahsan8751@gmail.com>
2025-12-04 04:21:38 -0800

    Simple Queue Operations

---
+++ Queue/01_SimpleQueue/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<int> numbers = new Queue<int>();
+
+       //Enqueue -> Add elements
+        numbers.Enqueue(10);
+        numbers.Enqueue(20);
+        numbers.Enqueue(30);
+
+        // Print Numbers after Enqueue 
+        Console.WriteLine("Queue Elements are:");
+        foreach(int n in numbers)
+        {
+           Console.WriteLine(n);
+        }
+
+        //Check first element without removing
+        Console.WriteLine("\nPeek Elements: "+ numbers.Peek());
+
+        //Dequeue -> Remove the first element
+        Console.WriteLine("\nDequeue: "+ numbers.Dequeue());
+
+        //Count all elements
+        Console.WriteLine("\nTotal elements are:"+ numbers.Count());
+
+        Console.WriteLine("\nQueue after Dequeue:");
+        foreach(int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
