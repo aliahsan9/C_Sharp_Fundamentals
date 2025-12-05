d7d5b6e133b13f7e8a9e6cfabc10d0e880550adc
Ali Ahsan <aliahsan8751@gmail.com>
2025-12-05 04:58:45 -0800

    Valid parantheses using stack

---
+++ Stack/03_Balanced_Parantheses/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+class BalancedParantheses
+{
+    public static bool IsBalanced(string input)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        foreach(char ch in input)
+        {
+            //If it's an opening bracket, push it in stack
+            if(ch == '(' || ch == '{' || ch == '[')
+            {
+                stack.Push(ch);
+            }
+            //If it's cl;osing bracket 
+            else 
+            if(ch == ')' || ch == '}' || ch == ']')
+            {
+                //if the stack is empty -> no opening bracket
+                if(stack.Count == 0)
+                {
+                    return false;
+                }
+                char top = stack.Pop();
+
+                //Check matching pairs
+                if((ch == ')' && ch == '(') ||
+                (ch == '}' && ch == '{') ||
+                (ch == ']' && ch == '['))
+                {
+                    return false;
+                }
+
+            }
+        }
+        //Stack must be empty -> all brackets matched
+        return stack.Count == 0;
+    }
+    static void Main(String[] args)
+    {
+        Console.Write("Enter Expression: ");
+        string input = Console.ReadLine();
+
+        Console.WriteLine(IsBalanced(input) ? "Balanced" : "Not Balanced");
+
+    }
+}
