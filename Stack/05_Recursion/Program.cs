13d4954c3ac30136fc56f3ae094f90212ef6659a
Ali Ahsan <aliahsan8751@gmail.com>
2025-12-06 04:24:37 -0800

    Recursion using stack

---
+++ Stack/05_Recursion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class RecursionStackDemo
+{
+   static int Factorial(int n)
+   {
+      //Base Case
+      if(n == 0)
+      {
+        return 1;
+      }
+      return n * Factorial(n - 1);
+   }
+   static void Main(String[] args)
+   {
+    Console.WriteLine("Factorial of 5 = " + Factorial(5));
+   }
+}
