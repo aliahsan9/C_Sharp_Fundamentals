e4bb556f485eec32c88d2e84a9a325da1935f3af
Ali Ahsan <aliahsan8751@gmail.com>
2025-12-06 04:35:38 -0800

    Recursion using stack

---
+++ Stack/06_ManualRecursion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ManualRecursion
+{
+    public static int RecursionStack(int n)
+    {
+
+        Stack<int> stack = new Stack<int>();
+        // push n into stack
+        while(n > 1)
+        {
+            stack.Push(n);
+            n--;
+        }
+        int result = 1;
+
+        while(stack.Count > 0)
+        {
+            result *= stack.Pop();
+        }
+        return result;
+    }
+    static void Main(string[]args)
+    {
+        Console.WriteLine("Factorial of given number is: " + RecursionStack(5));
+    }
+}
