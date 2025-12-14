345b7359d9d301c40817422c4f5ec7c14e857c97
Ali Ahsan <aliahsan8751@gmail.com>
2025-12-13 21:41:54 -0800

    prime numbers

---
+++ Math/02_PrimeNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class PrimeNumbers
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter any Number:");
+        int num = int.Parse(Console.ReadLine());
+
+        bool isPrime = true;
+
+        if(num <= 1)
+        {
+            isPrime = false;
+        }
+        else
+        {
+            for(int i = 2; i * i <= num; i++)
+            {
+                if(num % i == 0)
+                isPrime = false;
+                break;
+            }
+        }
+    if(isPrime)
+    Console.WriteLine($"{num} is Prime number.");
+    else
+    Console.WriteLine($"{num} is not a prime number.");
+}
+}
