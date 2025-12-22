55a85557f17c90c3d78b4d87d5ec47a7a475c2ff
Ali Ahsan <aliahsan8751@gmail.com>
2025-12-22 08:10:46 -0800

    added new brute force probkem

---
+++ HashMap/01_TwoSum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class TwoSum
+{
+    static int[] TwoSumMethod(int[] nums, int target)
+    {
+        // Brute force approach
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                int sum = nums[i] + nums[j];
+
+                if (sum == target)
+                {
+                    return new int[] { i, j };
+                }
+            }
+        }
+
+        return new int[] { -1, -1 };
+    }
+
+    static void Main(string[] args)
+    {
+        int[] arr = { 2, 7, 8, 9, 10 };
+
+        int[] result = TwoSumMethod(arr, 109);
+
+        Console.WriteLine($"Indices are: {result[0]}, {result[1]}");
+    }
+}
