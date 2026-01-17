4477666c330b626708babb29148c82bf820b053b
Ali Ahsan <aliahsan8751@gmail.com>
2026-01-17 09:33:31 +0500

    Added new Prime Numbers logic

---
+++ Math/03_PrimeNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+class PrimeNumbers
+{
+    public static int CheckPrime(int num)
+    {
+
+        if(num <= 1)
+        {
+            return 0; // Not Prime
+        }
+        for(int i = 2; i * i <= num; i++)
+        {
+            if(num % i == 0)
+            {
+                return 0; // Not Prime
+            }
+        }
+        return 1; // prime
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        int num = 4;
+        if(PrimeNumbers.CheckPrime(num) == 1)
+        {
+            Console.WriteLine($"{num} is Prime Number!");
+        }
+        else
+        {
+            Console.WriteLine($"{num} is non-prime!");
+        }
+    }
+}
