2ec47b8675909cba282084eccdad391dfdbbf292
Ali Ahsan <aliahsan8751@gmail.com>
2026-01-22 06:54:24 +0500

    Added new logic in prime numbers

--- Math/03_PrimeNumbers/Program.cs
+++ Math/03_PrimeNumbers/Program.cs
@@ -23,7 +23,7 @@ class Program
 {
     public static void Main()
     {
-        int num = 4; //Initialize num 4 now this is
+        int num = 4; //Initialize num 4 
         if(PrimeNumbers.CheckPrime(num) == 1)
         {
             Console.WriteLine($"{num} is Prime Number!");
