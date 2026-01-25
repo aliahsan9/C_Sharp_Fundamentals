e4da4eadd1b74e43e09f240565624596cdb73590
Ali Ahsan <aliahsan8751@gmail.com>
2026-01-25 21:23:35 +0500

    Added new problem from basics

---
+++ C#_Basics/28_StringInterpolation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    string name = "Ali Ahsan";
+    int age = 18;
+    string country = "Pakistan";
+
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Age: {age}");
+        Console.WriteLine($"Country: {country}");
+
+    }
+}
