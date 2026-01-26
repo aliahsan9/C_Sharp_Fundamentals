54994793d98525ced7906265116da741974c3dd8
Ali Ahsan <aliahsan8751@gmail.com>
2026-01-26 10:48:19 +0500

    Vowels and Consonants

---
+++ C#_Basics/36_VowelsConsonants/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class VowelsConsonants
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter any string:");
+        string input = Console.ReadLine();
+
+        int vowels = 0, consonants = 0, digits = 0;
+
+        foreach(char ch in input.ToLower())
+        {
+            if(char.IsDigit(ch))
+            {
+                digits++;
+            }
+            else
+                if(ch >= 'a' && ch <= 'z')
+            {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                    vowels++;
+                else
+                    consonants++;
+            }
+        }
+        Console.WriteLine($"Vowels are: {vowels}");
+        Console.WriteLine($"Consonants are: {consonants}");
+        Console.WriteLine($"Digits are: {digits}");
+    }
+
+}
