610dd756206ab3fddebd73c43c9aacab00ec8451
Ali Ahsan <aliahsan8751@gmail.com>
2026-01-26 09:47:57 +0500

    Fibonacci Numbers

---
+++ C#_Basics/33_SwitchOperator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter marks: ");
+        int marks = int.Parse(Console.ReadLine());
+
+        switch (marks / 10)
+        {
+            case 10:
+            case 9:
+                Console.WriteLine("Grade: A");
+                break;
+
+            case 8:
+                Console.WriteLine("Grade: B");
+                break;
+
+            case 7:
+                Console.WriteLine("Grade: C");
+                break;
+
+            case 6:
+                Console.WriteLine("Grade: D");
+                break;
+
+            default:
+                Console.WriteLine("Grade: F");
+                break;
+        }
+    }
+}
---
+++ C#_Basics/34_FibonacciNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class FibonacciNumbers
+{
+    static void Main(string[] args)
+    {
+        int nums = 10;
+        int a = 0, b = 1;
+
+        // Fibonacci Series
+        Console.WriteLine("Fibonacci Series is:");
+        for(int i = 0; i< nums; i++)
+        {
+            Console.Write(a + " ");
+
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
