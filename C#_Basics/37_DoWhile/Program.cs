46de6fcc0fe4bb5b2491c91a12d11a0a10b24cd7
Ali Ahsan <aliahsan8751@gmail.com>
2026-01-26 11:17:11 +0500

    Menu Driven Usinf Do While

---
+++ C#_Basics/37_DoWhile/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class MenuDriven
+{
+    static void Main(String[] args)
+    {
+        int choice = 0;
+        do
+        {
+            // Menu
+            Console.WriteLine("--- Menu ---");
+            Console.WriteLine("1. Add Two numbers:");
+            Console.WriteLine("2. Subtract Two numbes:");
+            Console.WriteLine("3. Multiply Two numbers:");
+            Console.WriteLine("4. Exit!");
+
+            // Read Choice
+            choice = int.Parse(Console.ReadLine() ?? "0");
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter a1:");
+                    int a1 = int.Parse(Console.ReadLine() ?? "0");
+                    Console.WriteLine("Enter b1:");
+                    int b1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"Result: {a1 + b1}");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Enter a2:");
+                    int a2 = int.Parse(Console.ReadLine() ?? "0");
+                    Console.WriteLine("Enter b2:");
+                    int b2 = int.Parse(Console.ReadLine() ?? "0");
+                    Console.WriteLine($"Result: {a2 - b2}");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Enter a3:");
+                    int a3 = int.Parse(Console.ReadLine() ?? "0");
+                    Console.WriteLine("Enter b3");
+                    int b3 = int.Parse(Console.ReadLine() ?? "0");
+                    Console.WriteLine($"Result: {a3 * b3}");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Exiting!");
+                    break;
+
+                default:
+                    Console.WriteLine("InValid Input!");
+                    break;
+
+            }
+        }
+        while (choice != 4);
+    }
+}
