285d3c8622256f2c3098b80de96e98ded9f6d296
Ali Ahsan <aliahsan8751@gmail.com>
2026-01-26 14:57:17 +0500

    Math Utility

---
+++ C#_Basics/41_OverloadingMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class MathUtility
+{
+   public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+   public static double Add(double a, double b)
+    {
+        return a + b;
+    }
+   public static int Add(int a, int b, int c)
+    {
+        return a + b + c; 
+    }
+}
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Two Integers:" + MathUtility.Add(3, 4));
+        Console.WriteLine("Two Doubles:" + MathUtility.Add(3.5, 6.5));
+        Console.WriteLine("Three Integers:" + MathUtility.Add(3, 4, 5));
+    }
+}
