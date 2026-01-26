32c8bb9d1dd48ffb69df46dcba88e101a68f9c60
Ali Ahsan <aliahsan8751@gmail.com>
2026-01-26 09:14:58 +0500

    Evem ODD AND PRIME check

---
+++ C#_Basics/31_EvenOddPrime/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class CheckNumbers
+{
+    static void Main(string[] args)
+    {
+        int n = 13;
+
+        if(n % 2 == 0)
+        {
+            Console.WriteLine($"{n} is Even Number.");
+        }
+        else
+        {
+            Console.WriteLine($"{n} is Odd Number.");
+        }
+        bool isPrime = false;
+        if(n <= 0)
+        {
+            isPrime = false;
+        }
+        else
+        {
+            for(int i = 2; i<= n / 2;  i++)
+            {
+                if(n % i == 0)
+                {
+                    isPrime = false;
+                }
+            }
+        }
+        if (isPrime)
+        {
+            Console.WriteLine($"{n} is Prime");
+        }
+        else
+        {
+            Console.WriteLine($"{n} is Not Prime!");
+        }
+    }
+}
