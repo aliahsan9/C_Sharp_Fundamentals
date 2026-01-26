71b7af5b2d9180c241234f2164f4df32680cbdd2
Ali Ahsan <aliahsan8751@gmail.com>
2026-01-26 10:10:39 +0500

    Palidrome Numbers

---
+++ C#_Basics/35_PalidromeNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int originalNumber = number;
+        int reverse = 0;
+
+        // Reverse the number
+        while (number > 0)
+        {
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number = number / 10;
+        }
+
+        Console.WriteLine($"Reversed number: {reverse}");
+
+        // Palindrome check
+        if (originalNumber == reverse)
+            Console.WriteLine("The number is a Palindrome");
+        else
+            Console.WriteLine("The number is Not a Palindrome");
+    }
+}
