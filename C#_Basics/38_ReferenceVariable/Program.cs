39b046acb46e0dc0eb6ee88d9cf1da55c558e745
Ali Ahsan <aliahsan8751@gmail.com>
2026-01-26 13:31:41 +0500

    Reference

---
+++ C#_Basics/38_ReferenceVariable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    // Method using ref
+    static void DoubleRef(ref int number)
+    {
+        number = number * 2;
+    }
+
+    // Method using out
+    static void GetSumAndProduct(int a, int b, out int sum, out int product)
+    {
+        sum = a + b;
+        product = a * b;
+    }
+
+    static void Main()
+    {
+        // ----------- ref demonstration -----------
+        int num = 5;
+        Console.WriteLine("Before ref method: " + num);
+
+        DoubleRef(ref num); // pass using ref
+        Console.WriteLine("After ref method: " + num);
+
+        // ----------- out demonstration -----------
+        int x = 4, y = 6;
+        int sumResult, productResult;
+
+        GetSumAndProduct(x, y, out sumResult, out productResult); // pass using out
+        Console.WriteLine($"Sum: {sumResult}, Product: {productResult}");
+    }
+}
