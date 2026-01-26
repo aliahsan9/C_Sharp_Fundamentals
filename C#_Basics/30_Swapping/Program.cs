380a6d2ce3d51717863a7889d702138c17473950
Ali Ahsan <aliahsan8751@gmail.com>
2026-01-26 09:05:41 +0500

    Swapping numbers without third variable

---
+++ C#_Basics/30_Swapping/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Swapping
+{
+    static void Main(string[] args)
+    {
+        int a = 5, b = 10;
+        a = a + b; // 5 + 10 = 15
+        b = a - b; // 15 - 10 = 5
+        a = a - b; // 15 - 5 = 10
+        Console.Write($"a is: {a} and b is: {b}");
+    }
+}
