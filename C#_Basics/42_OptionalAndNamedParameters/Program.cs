940726f6d9618a69ec92f1919275c3317f175fca
Ali Ahsan <aliahsan8751@gmail.com>
2026-01-26 15:26:15 +0500

    Optional and named parameters

---
+++ C#_Basics/42_OptionalAndNamedParameters/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void DisplayInfo(string name, int age = 18, string country = "Pakistan")
+    {
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Age: {age}");
+        Console.WriteLine($"Country: {country}");
+
+    }
+    static void Main(string[] args)
+    {
+        // Using all parameters
+        DisplayInfo("Aslam", 20, "UK");
+
+        Console.WriteLine();
+
+        // Use Optional Parameter
+        DisplayInfo("Ahmad");
+
+        // Using named parameters (order does'nt matter)
+        DisplayInfo(
+            "Sara",
+            34,
+            "India"
+            );
+    }
+}
