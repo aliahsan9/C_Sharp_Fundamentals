07e5e99feb51109e5215a3b3308c71f0fd4c2c02
Ali Ahsan <aliahsan8751@gmail.com>
2026-01-26 09:26:20 +0500

    Check largest number

---
+++ C#_Basics/32_LargestNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class LargestNumber
+{
+    static void Main(string[] args)
+    {
+        int a = 5, b = 10, c = 15;
+
+        // Using Conditional Operators
+         int largest = (a > b && a > c) ? a 
+            : (b > c) ? b 
+            : c;
+        Console.WriteLine($"Largest number is: {largest}");
+    }
+}
