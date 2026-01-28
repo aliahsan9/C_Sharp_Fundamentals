e25d1fd9fcda59857a048c1f44819132510e3b18
Ali Ahsan <aliahsan8751@gmail.com>
2026-01-28 22:53:48 +0500

    String vs StringBuilder Test

---
+++ C#_Basics/53_StringBuilder/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        const int iterations = 100_000;
+
+        // -------- String Test ----------
+        Stopwatch swString = Stopwatch.StartNew();
+
+        string result = "";
+        for (int i = 0; i < iterations; i++)
+        {
+            result += i;
+        }
+
+        swString.Stop();
+
+        Console.WriteLine($"String Iterations Time: {swString.ElapsedMilliseconds} ms");
+
+        // ------- StringBuilder Test ------------
+        Stopwatch swBuilder = Stopwatch.StartNew();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < iterations; i++)
+        {
+            sb.Append(i);
+        }
+        string finalResult = sb.ToString();
+
+        swBuilder.Stop();
+        Console.WriteLine($"StringBuilder Time: {swBuilder.ElapsedMilliseconds} ms");
+    }
+}
