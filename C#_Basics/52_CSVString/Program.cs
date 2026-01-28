f34ba4b575a4ee8250177ec07919397fdd49ad20
Ali Ahsan <aliahsan8751@gmail.com>
2026-01-28 18:24:08 +0500

    CSV --> Comman Separated Values

---
+++ C#_Basics/52_CSVString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string csv = "Ali, 25, Developer, Karachi";
+
+        // Split the CSV string
+        string[] values = csv.Split(',');
+
+        Console.WriteLine("Name: " + values[0]);
+        Console.WriteLine("Age: " + values[1]);
+        Console.WriteLine("Profession: " + values[2]);
+        Console.WriteLine("City: " + values[3]);
+
+    }
+}
