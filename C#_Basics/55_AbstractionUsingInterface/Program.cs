5460ff3aa31e7b2a73935327ff563f67dbb85da2
Ali Ahsan <aliahsan8751@gmail.com>
2026-01-29 16:06:10 +0500

    Abstraction concept using interface

---
+++ C#_Basics/55_AbstractionUsingInterface/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+interface IStudent
+{
+   void say();
+}
+class ClassRoom: IStudent
+{
+    public void say(){
+        Console.WriteLine("Hello, how are you!");
+}
+}
+class Program
+{
+    static void Main(String[] args)
+    {
+        IStudent obj = new ClassRoom();
+        obj.say();
+    }
+}
