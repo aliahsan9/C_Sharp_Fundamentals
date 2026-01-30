58e35b8cef5373b88fa265552e60d953df06a872
Ali Ahsan <aliahsan8751@gmail.com>
2026-01-30 12:00:42 +0500

    Interfaces implementation in classes

---
+++ C#_Basics/59_MultipleInterfaces/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+
+public interface ILogger
+{
+    void Log(string message);
+}
+public interface ICalculator
+{
+    int Add(int a, int b);
+}
+
+public class UtilityService: ILogger, ICalculator
+{
+    public void Log(string message)
+    {
+        Console.WriteLine($"message");
+    }
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+class Program
+{
+  static void Main(String[] args)
+  {
+        UtilityService service = new UtilityService();
+
+        service.Log("Application Started.");
+        int result = service.Add(5, 10);
+
+        Console.WriteLine($"Result: {result}");
+  }
+}
