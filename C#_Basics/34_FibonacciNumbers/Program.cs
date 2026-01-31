d04405dc79ff26a0f3ad19f442d2dc96f23b33a3
Ali Ahsan <aliahsan8751@gmail.com>
2026-01-31 09:01:19 +0500

    Fibonacci Numbers

--- C#_Basics/34_FibonacciNumbers/Program.cs
+++ C#_Basics/34_FibonacciNumbers/Program.cs
@@ -5,16 +5,21 @@ class FibonacciNumbers
     static void Main(String[] args)
     {
         int nums = 10;
-        int a = 0, b = 1;
+        int a = 0, b = 1; // First Two Fibonacci Numbers
 
         Console.WriteLine("Fibonacci Numbers:");
         for(int i = 0; i <= nums; i++)
         {
-            Console.Write(a + " ");
+            Console.Write(a + " "); // a = 0
 
-            int next = a + b;
-            a = b;
-            b = next;
+            int next = a + b;   // 0 + 1 = 1
+
+            //Move b into a
+            a = b;         
+            
+            // Move next into b
+            b = next;          
+            
         }
     }
 }
