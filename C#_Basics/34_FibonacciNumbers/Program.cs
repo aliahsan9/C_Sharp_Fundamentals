08459f5316b9743a9e29bf62f0344bce37334715
Ali Ahsan <aliahsan8751@gmail.com>
2026-01-31 08:57:06 +0500

    Fibonacci Numbers

--- C#_Basics/34_FibonacciNumbers/Program.cs
+++ C#_Basics/34_FibonacciNumbers/Program.cs
@@ -2,14 +2,13 @@
 
 class FibonacciNumbers
 {
-    static void Main(string[] args)
+    static void Main(String[] args)
     {
         int nums = 10;
         int a = 0, b = 1;
 
-        // Fibonacci Series
-        Console.WriteLine("Fibonacci Series is:");
-        for(int i = 0; i< nums; i++)
+        Console.WriteLine("Fibonacci Numbers:");
+        for(int i = 0; i <= nums; i++)
         {
             Console.Write(a + " ");
 
