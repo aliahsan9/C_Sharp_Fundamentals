3e7b2a94024c08d8fc3e1c4a5a233880eebb3632
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-03 08:47:44 +0500

    Immutable class

---
+++ C#_Basics/64_ImmutableClass/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+sealed class Student
+{
+    private readonly string _name;
+    private readonly int _id;
+    private readonly string _city;
+
+    // Constructor
+    public Student(string name, int id, string city)
+    {
+        _name = name;
+        _id = id;
+        _city = city;
+    }
+    // Getters method
+    public string Name
+    {
+        get { return _name; }
+    }
+    public int Id
+    {
+        get { return _id; }
+    }
+    public string City
+    {
+        get { return _city; }
+    }
+    // Display info
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {Name}, Id: {Id}, City: {City}");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating immutable object
+        Student s = new Student("Ali Ahsan", 101, "Lahore");
+        s.DisplayInfo();
+    }
+}
