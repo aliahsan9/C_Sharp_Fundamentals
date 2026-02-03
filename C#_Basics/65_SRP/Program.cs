929a6a23c481527f2b36412b557284c75cb43cfe
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-03 09:20:03 +0500

    SRP

---
+++ C#_Basics/65_SRP/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Student
+{
+    public string Name;
+    public int Id;
+    public string City;
+
+    public Student(string name, int id, string city)
+    {
+        Name = name;
+        Id = id;
+        City = city;
+    }
+}
+class PrintStudent
+{
+    public void Print(Student student)
+    {
+        Console.WriteLine($"Student's Name: {student.Name}, Id: {student.Id} and City: {student.City}");
+    }
+}
+class StudentRepository
+{
+    public void Save(Student student)
+    {
+        Console.WriteLine("Student's data saved in Database");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student student = new Student("Ali Ahsan", 101, "Lahore");
+
+        PrintStudent p = new PrintStudent();
+        StudentRepository r = new StudentRepository();
+
+        p.Print(student);
+        r.Save(student);
+    }
+}
