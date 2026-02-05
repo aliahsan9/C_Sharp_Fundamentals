56eb9dc049d5539f825ec31df928c969c6262eba
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-05 09:00:03 +0500

    remove duplicates using hashset

---
+++ C#_Basics/69_RemoveDuplicates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int>
+        {
+            1, 2, 3, 2, 4, 1, 5, 3
+        };
+
+        // HashSet automatically removes duplicates
+        HashSet<int> uniqueNumbers = new HashSet<int>(numbers);
+
+        Console.WriteLine("After removing duplicates:");
+        foreach (int number in uniqueNumbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
