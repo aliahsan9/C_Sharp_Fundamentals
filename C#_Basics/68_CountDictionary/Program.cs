0b7c073df0471b4942e7bb8f9690600410515ffb
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-05 08:37:52 +0500

    count words

---
+++ C#_Basics/68_CountDictionary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string text = "hello world hello dotnet world";
+
+        // Dictionary to store word frequency
+        Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+        // Split text into words
+        string[] words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (string word in words)
+        {
+            // Convert to lowercase to avoid case issues
+            string key = word.ToLower();
+
+            // If word already exists, increase count
+            if (wordCount.ContainsKey(key))
+            {
+                wordCount[key]++;
+            }
+            // Otherwise, add new word with count = 1
+            else
+            {
+                wordCount.Add(key, 1);
+            }
+        }
+
+        // Print result
+        Console.WriteLine("Word Frequency:");
+        foreach (var item in wordCount)
+        {
+            Console.WriteLine($"{item.Key} -> {item.Value}");
+        }
+    }
+}
