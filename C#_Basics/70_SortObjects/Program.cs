7ce0f2a3b43c1dd8f9e708f5097253d6c8e7cac6
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-06 09:40:57 +0500

    sort objects

---
+++ C#_Basics/70_SortObjects/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Student : IComparable<Student>
+{
+    public string Name;
+    public int Marks;
+
+    public Student(string name, int marks)
+    {
+        Name = name;
+        Marks = marks;
+    }
+
+    public int CompareTo(Student other)
+    {
+        if (other == null)
+            return 1;
+
+        return this.Marks.CompareTo(other.Marks);
+    }
+}
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        List<Student> students = new List<Student>
+      {
+        new Student ("Ali", 70),
+        new Student ("Ahmad", 66),
+        new Student ("Imran", 88)
+       };
+
+        students.Sort();
+
+        Console.WriteLine("Students Sorted by marks:");
+
+        foreach(var student in students)
+        {
+            Console.WriteLine($"{student.Name}, {student.Marks}");
+        }
+    }
+}
