1257e7ee5923b9dc7b7806de91330e47223b9514
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-06 13:47:04 +0500

    Sum of unique numbers

---
+++ HashMap/02_SumUniqueNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int SumOfUnique(int[] nums)
+    {
+        // Dictionary to store frequency of each number
+        Dictionary<int, int> count = new Dictionary<int, int>();
+
+        // Count frequencies
+        foreach (int num in nums)
+        {
+            if (count.ContainsKey(num))
+                count[num]++;
+            else
+                count[num] = 1;
+        }
+
+        // Sum numbers that appear exactly once
+        int result = 0;
+        foreach (var kvp in count)
+        {
+            if (kvp.Value == 1)
+                result += kvp.Key;
+        }
+
+        return result;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 1, 2, 3, 2 };
+        Solution sol = new Solution();
+        int result = sol.SumOfUnique(nums);
+        Console.WriteLine($"Sum of unique elements: {result}");
+    }
+}
