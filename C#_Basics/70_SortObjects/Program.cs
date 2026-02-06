7182a05668fdafb4addd2c3b11d5e3f79ba977a1
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-06 09:41:56 +0500

    sort objects

--- C#_Basics/70_SortObjects/Program.cs
+++ C#_Basics/70_SortObjects/Program.cs
@@ -1,44 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 
+// Student class implementing IComparable
+// This allows objects of Student to be sorted
 class Student : IComparable<Student>
 {
-    public string Name;
-    public int Marks;
+    public string Name { get; set; }
+    public int Marks { get; set; }
 
+    // Constructor
     public Student(string name, int marks)
     {
         Name = name;
         Marks = marks;
     }
 
+    // CompareTo method defines how objects will be compared
+    // This method is automatically used by Sort()
     public int CompareTo(Student other)
     {
+        // Safety check
         if (other == null)
             return 1;
 
+        // Sorting logic:
+        // Compare students based on Marks
+        // Ascending order (smallest to largest)
         return this.Marks.CompareTo(other.Marks);
+
+        // For descending order:
+        // return other.Marks.CompareTo(this.Marks);
     }
 }
 
 class Program
 {
-    static void Main(String[] args)
+    static void Main()
     {
+        // Create a list of students
         List<Student> students = new List<Student>
-      {
-        new Student ("Ali", 70),
-        new Student ("Ahmad", 66),
-        new Student ("Imran", 88)
-       };
+        {
+            new Student("Ali", 75),
+            new Student("Sara", 90),
+            new Student("Ahmed", 60),
+            new Student("Zara", 85)
+        };
 
+        // Sort the list
+        // This automatically calls CompareTo()
         students.Sort();
 
-        Console.WriteLine("Students Sorted by marks:");
-
-        foreach(var student in students)
+        // Display sorted students
+        Console.WriteLine("Students sorted by marks (ascending):");
+        foreach (var student in students)
         {
-            Console.WriteLine($"{student.Name}, {student.Marks}");
+            Console.WriteLine(student.Name + " - " + student.Marks);
         }
     }
 }
