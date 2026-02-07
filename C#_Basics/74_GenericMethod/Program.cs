9f5b4e7cd75dbc10c96c2ff5a0885b424512a554
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-07 18:12:32 +0500

    Generic method simple

---
+++ C#_Basics/74_GenericMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    public static void PrintValue<T>(T value)
+    {
+          Console.WriteLine("Value is:"+ value);    
+    }  
+    static void Main(string[] args)
+        {
+        // Printing Integer value
+
+        PrintValue<int>(12);
+
+        // Printing String value
+        PrintValue<string>("Hello");
+
+        // Printing double value
+        PrintValue<double>(3.14);
+
+        // Printing boolean value
+        PrintValue<bool>(true);
+        }
+}
