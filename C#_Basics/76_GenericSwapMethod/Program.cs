4f8a32cb04689fe4a90b26b9bc8a688f377460b8
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-07 18:47:41 +0500

    generic method using ref

---
+++ C#_Basics/76_GenericSwapMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{ 
+    public static void Swap<T>(ref T a, ref T b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+    static void Main(string[] args)
+    {
+        int x = 5, y = 10;
+
+        Console.WriteLine($"Before Swapping: x = {x}, y = {y}");
+
+        Swap<int>(ref x, ref y);
+
+        Console.WriteLine($"After Swapping: x = {x}, y = {y}");
+    }
+}
