bbe9f106e336a349e71fb3a1fa7fc0e1b870bb8c
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-10 11:59:10 +0500

    Span elemenst

---
+++ C#_Basics/80_Span/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        int[] nums = { 10, 20, 30, 40, 50 };
+
+        Span<int> span = nums;
+
+        span[0] = 70;
+
+        foreach(var num in nums)
+        {
+            Console.WriteLine(num);
+        }
+        Span<int> slice = span.Slice(1, 3);
+
+        foreach(var num in slice)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
