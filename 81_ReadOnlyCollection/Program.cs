8dc8df607a0972f5620739bfd6ad9c90b7941762
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-10 12:15:19 +0500

    readonly collection code

---
+++ 81_ReadOnlyCollection/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        List<string> fruits = new List<string>()
+        {
+            "Apple",
+            "Orange",
+            "Banana"
+        };
+        // Create a readonly collection from the list
+        ReadOnlyCollection<string> readonlyfruits = 
+            new ReadOnlyCollection<string>(fruits);
+
+       // Display items from the readonly list
+       foreach(string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+    }
+}
