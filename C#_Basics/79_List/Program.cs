9d3b2d766185cb9a6ecbda5740231aacd9663411
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-10 10:56:00 +0500

    List elements

---
+++ C#_Basics/79_List/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> nums = new List<int>();
+
+        nums.Add(10);
+        nums.Add(20);
+
+        nums.Remove(10);
+        foreach (int num in nums)
+        {
+            Console.WriteLine(num);
+        }
+
+    }
+}
