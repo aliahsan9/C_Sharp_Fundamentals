400041f72f2a20a4f5600dbbd0cea119f63e83cf
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-10 10:50:19 +0500

    Array elements

---
+++ C#_Basics/78_ListArraySpan/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        int[] array = new int[3];
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 30;
+
+        for(int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine("Array Elements are:"+ array[i]);
+        }
+    }
+}
