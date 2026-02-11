b9f9879775da6a64cafc88f728d7fa8d3ba26f9e
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-11 09:57:02 +0500

    Single Threading

---
+++ C#_Basics/82_SingleThread/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        static void PrintNumbers(){
+        for(int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine(i);
+                Thread.Sleep(500);
+        }
+       }
+        static void PrintLetters()
+        {
+            for(char c = 'A'; c < 'E'; c++)
+            {
+                Console.WriteLine(c);
+                Thread.Sleep(500);
+            }
+        }
+        PrintLetters();
+        PrintNumbers();
+    }
+}
