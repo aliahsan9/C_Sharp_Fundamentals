a4902e5d9fb21e8765c20667917bac346ac5a36e
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-11 18:21:25 +0500

    Use of all, any and contains

---
+++ C#_Basics/87_AnyAllContains/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        List<int> numbers = new List<int> { 10, 30, 77, 88, 89, 100 };
+
+        // Use of Any
+        bool AnyGreaterThan90 = numbers.Any(n => n > 90);
+
+        Console.WriteLine(AnyGreaterThan90);
+
+        // Use of All
+        bool AllGreaterThan90 = numbers.All(n => n > 90);
+        Console.WriteLine(AllGreaterThan90);
+
+        // Use of Contains
+        bool Contains = numbers.Contains(10);
+
+        Console.WriteLine(Contains);
+    }
+}
