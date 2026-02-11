3c36b64a6e27857cd2a48fc82478317a8be9e9b2
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-11 18:33:08 +0500

    flattened lists into single list

---
+++ C#_Basics/88_SelectMany/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Nested list (list of lists)
+        List<List<int>> numbers = new List<List<int>>
+        {
+            new List<int> { 1, 2 },
+            new List<int> { 3, 4 },
+            new List<int> { 5, 6 }
+        };
+
+        // Flatten the nested lists into a single list
+        var flattened = numbers
+            .SelectMany(innerList => innerList);
+
+        // Print flattened result
+        Console.WriteLine("Flattened numbers:");
+        foreach (var num in flattened)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
