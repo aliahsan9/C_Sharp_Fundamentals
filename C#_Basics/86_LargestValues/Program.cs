7a5139cdcca5844db61179f6f4a66b972915f133
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-11 15:50:59 +0500

    take top 3 numbers using linq

---
+++ C#_Basics/86_LargestValues/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        List<int> numbers = new List<int> { 10, 30, 70, 50, 100, 80};
+
+        // Sort Numbers in ascending
+        // Now take top 3
+        var top3 = numbers
+            .OrderByDescending(n => n)
+            .Take(3);
+
+         // Now print results
+
+        foreach(var num in top3)
+        {
+            Console.WriteLine($"{num}");
+        }
+    }
+
+}
