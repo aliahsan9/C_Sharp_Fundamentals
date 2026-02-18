d08206003576b27d11316a98be1b62997e970faa
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-18 20:48:02 +0500

    added new logic

--- C#_Basics/01_C_Sharp/Program.cs
+++ C#_Basics/01_C_Sharp/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string [] srgs)
     {
-        Console.WriteLine("Hello, World! How are You?");        // hello w
+        Console.WriteLine("Hello, World! How are You?");        // h
     }
 }
