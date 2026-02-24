be2f0f1436b7bb55130936f128892679ff4445de
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-24 20:54:21 +0500

    Added Global Exception Middleware

---
+++ C#_Basics/96_GlobalException/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Text.Json;
+
+public class GlobalExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<GlobalExceptionMiddleware> _logger;
+
+    // Constructor
+    public GlobalExceptionMiddleware(RequestDelegate next, ILogger<GlobalExceptionMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            // Move to next middleware
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            // Log the error
+            _logger.LogError(ex, "An unhandled exception occurred.");
+
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private Task HandleExceptionAsync(HttpContext context, Exception ex)
+    {
+        HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+        string message = "Something went wrong.";
+
+        // Custom Exception Handling
+        if (ex is InvalidMarksException)
+        {
+            statusCode = HttpStatusCode.BadRequest;
+            message = ex.Message;
+        }
+
+        var response = new ErrorResponse
+        {
+            StatusCode = (int)statusCode,
+            Message = message,
+            Details = ex.Message
+        };
+
+        var result = JsonSerializer.Serialize(response);
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)statusCode;
+
+        return context.Response.WriteAsync(result);
+    }
+}
