049755ab025b51630426f65e0d4cc7a56fcff509
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-25 17:14:58 +0500

    Delegate

---
+++ C#_Basics/97_Delegate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public delegate int MyDelegate(int a, int b);
+
+public class Calculator
+{
+    // Method: 1
+    public int Add(int x, int y)
+    {
+        return x + y;
+    }
+    public int Multiply(int x, int y)
+    {
+        return x * y;
+    }
+}
+class Program
+{
+    static void Main(String[] args)
+    {
+        Calculator calc = new Calculator();
+
+        // Create a new Delegate
+        MyDelegate del;
+
+        del = calc.Add();
+        int result1 = del(5, 10);
+
+        Console.WriteLine("Sum is: " + result1);
+
+
+        del = calc.Multiply();
+        int result2 = del(5, 10);
+        Console.WriteLine("Multiplication Result:" + result2);
+    }
+}
